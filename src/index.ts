1b4f75ecb75bf25fa8e9befce5bc1c52bcddf4da
Josh Feinsilber <joshfeinsilber@gmail.com>
2019-03-19 08:15:46 -0700

    simple client export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import Client from './lib/room/Client'
 import Room from './lib/room/Room'
 import Server from './lib/server/Server'
+import SimpleClient from './types/SimpleClient'
 
 export interface EntityMap<T> {
-  [entityId: string]: T;
+  [entityId: string]: T
 }
 
 export { nonenumerable as nosync } from 'nonenumerable'
 
-export {Server, Room, Client}
+export { Server, Room, Client, SimpleClient }
